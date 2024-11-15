6804400024937db54ecee23750510e449f5f2b7c
kaiser-lai <klai2027@kehillahstudent.org>
2024-11-15 11:01:47 -0800

    done but I need some help on the factorial next class please？

--- ex1.py
+++ ex1.py
@@ -1,1 +1,15 @@
 # Use turtle graphics to make an infinite spiral
+from turtle import *
+f = forward
+r = right
+
+# repeat infinite 
+
+def vale():
+    move = 0.6
+    while True: 
+            move = move - 2
+            f(move)
+            r(90)
+            f(move)
+vale()
--- ex2.py
+++ ex2.py
@@ -1,1 +1,8 @@
 # make infinite circles, start with a small circle, then draw a bigger circle around it
+
+from turtle import *
+
+r = 1
+while True:
+    r = r * 2
+    circle(r)
